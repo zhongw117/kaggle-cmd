a070c632bdb04309811dad19db9b586ab6245ef1
Wilson <wilson@yesupmail.com>
2019-05-28 17:08:41 -0400

    Settings for Kaggle API

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+#
+# Copyright 2019 Kaggle Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# coding=utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name='kaggle',
+    version='1.5.4',
+    description='Kaggle API',
+    long_description=
+    ('Official API for https://www.kaggle.com, accessible using a command line '
+     'tool implemented in Python. Beta release - Kaggle reserves the right to '
+     'modify the API functionality currently offered.'),
+    author='Kaggle',
+    author_email='support@kaggle.com',
+    url='https://github.com/Kaggle/kaggle-api',
+    keywords=['Kaggle', 'API'],
+    entry_points={'console_scripts': ['kaggle = kaggle.cli:main']},
+    install_requires=[
+        # Restriction that urllib3's version is less than 1.25 needed to avoid
+        # requests dependency problem.
+        'urllib3 >= 1.21.1, < 1.25',
+        'six >= 1.10',
+        'certifi',
+        'python-dateutil',
+        'requests',
+        'tqdm',
+        'python-slugify'
+    ],
+    packages=find_packages(),
+    license='Apache 2.0')
